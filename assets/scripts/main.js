761f9bd70c681bf90a8f281b0c447bc4db15319c
Tassio <81255475+Tassio2002@users.noreply.github.com>
2023-02-12 17:12:56 -0300

    functions for buttons

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,7 +3,6 @@ let buyButton = document.querySelectorAll(".buy-btn");
 let card = document.querySelector(".t")
 let idx = 0;
 
-
 function showBuyButton(index) {
   buyButton[index].style.display = "block";
 }
