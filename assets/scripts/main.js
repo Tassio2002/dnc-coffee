c654d92621b0bec10fffd0717f9068c24dc9695a
Tassio <81255475+Tassio2002@users.noreply.github.com>
2023-02-12 17:12:39 -0300

    show button functions

---
+++ assets/scripts/main.js
@@ -0,0 +1,13 @@
+let cards = document.querySelectorAll(".info-card");
+let buyButton = document.querySelectorAll(".buy-btn");
+let card = document.querySelector(".t")
+let idx = 0;
+
+
+function showBuyButton(index) {
+  buyButton[index].style.display = "block";
+}
+
+function hideBuyButton(index) {
+  buyButton[index].style.display = "none";
+}
