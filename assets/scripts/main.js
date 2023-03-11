1a467dc4c321210e1f153b6dbb31d820d99673b8
Tassio <81255475+Tassio2002@users.noreply.github.com>
2023-03-11 11:19:46 -0300

    modal function

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,8 +1,14 @@
-let cards = document.querySelectorAll(".info-card");
-let buyButton = document.querySelectorAll(".buy-btn");
-let card = document.querySelector(".t")
+const cards = document.querySelectorAll(".info-card");
+const buyButton = document.querySelectorAll(".buy-btn");
+const card = document.querySelector(".t")
+const serviceLink = document.querySelector("#service-link")
+const blurModal = document.querySelector("#blur-container")
+const contactModal = document.querySelector("#modal-contact")
+const closeBtn = document.querySelector("#close")
+
 let idx = 0;
 
+// TODO: add event listeners
 function showBuyButton(index) {
   buyButton[index].style.display = "block";
 }
@@ -10,3 +16,16 @@ function showBuyButton(index) {
 function hideBuyButton(index) {
   buyButton[index].style.display = "none";
 }
+
+function showContactModal() {
+  blurModal.style.display = "flex"
+  contactModal.style.display = "flex"
+}
+
+function hideContactModal() {
+  contactModal.style.display = "none"
+  blurModal.style.display = "none"
+}
+
+serviceLink.addEventListener('click', showContactModal)
+closeBtn.addEventListener('click', hideContactModal)
